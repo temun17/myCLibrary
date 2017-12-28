e3f4b0b377b049c1b5465abaa310c07dde5db0f6
Allen Temunovic <allentemunovic@Allens-MacBook-Pro.local>
2017-12-28 15:25:43 -0700

    A few completed functions for libft

---
+++ libft/ft_memset.c
@@ -0,0 +1,13 @@
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char *s;
+
+	s = b;
+	while (len--)
+	{
+		*s++ = (unsigned char)c;
+	}
+	return (b);
+}
---
+++ libft/print_bits.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+void	print_bits(unsigned char octet)
+{
+	int oct;
+	int div;
+	
+	div = 128;
+	oct = octet;
+	while (div != 0)
+	{
+		if (div <= oct)
+		{
+			write(1, "1", 1);
+			oct = oct % div;
+		}
+		else
+		{
+			write(1, "0", 1);
+		}
+		div = div / 2;
+	}
+}
