3389c6e379ac377581997b71d5b2846f57593063
Allen Temunovic <ajtemunovic@gmail.com>
2018-02-19 13:05:17 -0800

    update functions

--- libft/srcs/memory/ft_memcmp.c
+++ libft/srcs/memory/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: atemunov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/19 12:37:40 by atemunov          #+#    #+#             */
-/*   Updated: 2018/02/19 12:37:44 by atemunov         ###   ########.fr       */
+/*   Updated: 2018/02/19 13:04:52 by atemunov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 int ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-    const unsigned char *str1;
-    const unsigned char *str2;
+	const unsigned char *str1;
+	const unsigned char *str2;
 
-    if (s1 == s2 || n == 0)
-        return (0);
-    str1 = (const unsigned char *)s1;
-    str2 = (const unsigned char *)s2;
-    while (n--)
-    {
-        if (*str1 != *str2)
-            return (*str1 - *str2);
-        if (n)
-        {
-            str1++;
-            str2++;
-        }
-    }
-    return (0);
+	if (s1 == s2 || n == 0)
+		return (0);
+	str1 = (const unsigned char *)s1;
+	str2 = (const unsigned char *)s2;
+	while (n--)
+	{
+		if (*str1 != *str2)
+			return (*str1 - *str2);
+		if (n)
+		{
+			str1++;
+			str2++;
+		}
+	}
+	return (0);
 }
